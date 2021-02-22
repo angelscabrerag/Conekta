43f577d605f9dae1ca1c618ecb0b3cdfc28b1955
Angel S. Cabrera <angel.cabrera@multieconomico.com.mx>
2021-02-22 02:46:40 -0700

    Update conektaspei.php
    Se agregan el id de transacción de conekta y el id de la factura al formulario para obtener el recibo de pago en SPEI
    
    Se agrega que se obtenga el error en caso de que no se pueda generar el cargo por error en la fecha.

--- modules/gateways/conektaspei.php
+++ modules/gateways/conektaspei.php
@@ -2,39 +2,39 @@
 
 // Copyright (c) 2015, Carlos Cesar Peña Gomez <CarlosCesar110988@gmail.com>
 //
-// Permission to use, copy, modify, and/or distribute this software for any 
-// purpose with or without fee is hereby granted, provided that the above copyright 
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above copyright
 // notice and this permission notice appear in all copies.
 
-// THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL 
-// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL 
-// THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL 
-// DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA 
-// OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR 
-// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE 
+// THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+// THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+// DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+// OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
 // USE OR PERFORMANCE OF THIS SOFTWARE.
 
 function conektaspei_config() {
     $configarray = array(
 		'FriendlyName' => array(
-			'Type' =>'System', 
+			'Type' =>'System',
 			'Value' =>'Conekta SPEI'
 		),
 		'private_key' => array(
-			'FriendlyName' => 'Llave Privada', 
-			'Type' => 'text', 
+			'FriendlyName' => 'Llave Privada',
+			'Type' => 'text',
 			'Size' => '50'
 		),
 		'days_expire' => array(
-			'FriendlyName' => 'Dias para Expirar', 
-			'Type' => 'text', 
+			'FriendlyName' => 'Dias para Expirar',
+			'Type' => 'text',
 			'Size' => '2',
 		),
 		'instructions' => array(
-			'FriendlyName' => 'Instrucciones de pago', 
-			'Type' => 'textarea', 
-			'Rows' => '5', 
+			'FriendlyName' => 'Instrucciones de pago',
+			'Type' => 'textarea',
+			'Rows' => '5',
 			'Description' => ''
 		)
     );
@@ -45,15 +45,15 @@ function conektaspei_link($params) {
 
 	# Variables de Conekta
 	$private_key = $params['private_key'];
-	
-	# Dias para que expire el Pago 
-	$days_expire = $params['days_expire'];
-	
+
+	# Dias para que expire el Pago
+	$days_expire = $params['days_expire'];
+
 	# Si el parametro viene vacio lo fijamos a 5 dias
 	if(strlen($days_expire)==0){$days_expire=5;}
-	
+
 	# AAAA-MM-DD
-	
+
 	$date 		= time();
 	$expires 	= date('Y-m-d', strtotime('+'. $days_expire .' day', $date));
 
@@ -75,26 +75,26 @@ function conektaspei_link($params) {
 	$phone 			= $params['clientdetails']['phonenumber'];
 
 	$results = array();
-	
+
 	# Preparamos todos los parametros para enviar a Conekta.io
-	
+
 	$data_amount 		= str_replace('.', '', $amount);
 	$data_currency 		= strtolower($currency);
-	$data_description 	= 'Pago Factura No. '.$invoiceid;
-	
+	$data_description 	= 'Pago de Factura No. '.$invoiceid.' en Hosting Fácil';
+
 	# Incluimos la libreria de Conecta 2.0
 
 	require_once('conekta/lib_2.0/Conekta.php');
-	
+
 	# Creamos el Objeto de Cargo
 	Conekta::setApiKey($private_key);
-	
+
 	# Arraglo con informacion de tarjeta
 	$conekta = array(
-				'description' 	=> $data_description, 
-				'reference_id' 	=> 'factura_'.$invoiceid, 
-				'amount' 		=> intval($data_amount), 
-				'currency' 		=> $data_currency, 
+				'description' 	=> $data_description,
+				'reference_id' 	=> 'factura_'.$invoiceid,
+				'amount' 		=> intval($data_amount),
+				'currency' 		=> $data_currency,
 				'bank' 			=> array(
 											'type'			=> 'spei',
 											'expires_at'	=> $expires
@@ -104,7 +104,7 @@ function conektaspei_link($params) {
 									      'phone'			=> $phone,
 									      'name'			=> $firstname.' '.$lastname,
 									      'line_items'		=> array(
-															        array( 
+															        array(
 															          'name'		=>	$data_description,
 															          'sku'			=>	$invoiceid,
 															          'unit_price'	=> 	intval($data_amount),
@@ -113,31 +113,45 @@ function conektaspei_link($params) {
 															          'type'		=>	'service-purchase'
 															        )
 									      )
-									      
+
 									 )
 				);
-				
+
 	try {
-	
-	
+
+
 	  $charge = Conekta_Charge::create($conekta);
-	  
+
 	  # Transaccion Correcta
 	  $data = json_decode($charge);
 
+	  $id_conekta		= $data->id;
 	  $clabe 			= $data->payment_method->clabe;
 	  $bank 			= $data->payment_method->bank;
 	  $type 			= $data->payment_method->type;
 	  $expires 		    = $data->payment_method->expires_at;
 
 	  $ticket = 1;
-	 
-	} 
-	
-	catch (Exception $e) 
+
+	}
+
+	catch (Exception $e)
 	{
-	  $code =  "Error al intentar generar CLAVE de SPEI";
-	  
+	  error_log('Error Conekta SPEI: Código: ' . $e->getLine() . '. ' . $e->getMessage() . '. URL: ' . $_SERVER[HTTP_HOST] . $_SERVER['REQUEST_URI'], 1, 'contacto@multieconomico.com.mx');
+	  if($e->getLine() == 35)
+	  {
+	  	$code =  "
+	  		<div class='alert alert-danger'>
+	  			Error al intentar generar la orden de pago mediante SPEI debido a que la fecha limite de pago vencio. Por favor, intenta con otra forma de pago.
+	  		</div>
+	  	";
+	  } else {
+	  	$code =  "
+	  		<div class='alert alert-danger'>
+	  			Error al intentar generar la orden de pago mediante SPEI. Por favor, intenta con otra forma de pago.
+	  		</div>
+	  	";
+	  }
 	  $ticket = 0;
 	}
 
@@ -149,10 +163,12 @@ function conektaspei_link($params) {
 		$code .= '<input type="hidden" name="expires" value="'. $expires .'" />';
 		$code .= '<input type="hidden" name="monto" value="'. $amount .'" />';
 		$code .= '<input type="hidden" name="concepto" value="'. $data_description .'" />';
-		$code .= '<input type="submit" value="Pagar ahora" />';
+		$code .= '<input type="hidden" name="id_processor" value="'. $id_conekta .'" />';
+		$code .= '<input type="hidden" name="id_invoice" value="'. $invoiceid .'" />';
+		$code .= '<input class="btn btn-success btn-block" type="submit" value="Pagar ahora" />';
 		$code .= '</form>';
 	}
-	
+
 	return $code;
 }
 
